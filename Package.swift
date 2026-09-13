03d61f077fbfb38d7b284c5b32dae7f0a5b0875c
pavandhakar <pavanpavandhakar@gmail.com>
2026-09-13 18:30:34 +0530

    bug fixed

--- Package.swift
+++ Package.swift
@@ -4,26 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "PDDropDownSwiftUI",
-    platforms: [
-            .iOS(.v17)
-        ],
+    name: "PDDropDownSwitfUI",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "PDDropDownSwiftUI",
-            targets: ["PDDropDownSwiftUI"]
+            name: "PDDropDownSwitfUI",
+            targets: ["PDDropDownSwitfUI"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PDDropDownSwiftUI"
+            name: "PDDropDownSwitfUI"
         ),
-        .testTarget(
-            name: "PDDropDownSwiftUITests",
-            dependencies: ["PDDropDownSwiftUI"]
-        ),
+
     ]
 )
